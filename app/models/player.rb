a5294f2434c2dfba4ef4daf939899e2d69fe8729
Bruno Casali <brunoocasali@gmail.com>
2014-11-05 18:07:36 -0200

    Removed the authentication by email and pass, only now its provide by oAuth2 with Twitter and Facebook.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,9 +1,5 @@
 class Player < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-
-  devise :database_authenticatable, :registerable, :recoverable,
-         :rememberable, :trackable, :validatable, :omniauthable
+  devise :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   validates_presence_of :email
 
