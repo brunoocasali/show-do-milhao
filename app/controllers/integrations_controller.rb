5d49b00899cf0a6277b35cbbec03a350e34687ae
Bruno Casali <brunoocasali@gmail.com>
2014-11-06 01:11:57 -0200

    Adding the juntar roles to make this works,

--- app/controllers/integrations_controller.rb
+++ app/controllers/integrations_controller.rb
@@ -1,12 +1,40 @@
 class IntegrationsController < ApplicationController
+  before_action :set_question, only: :update
+
   def index
     set_objects
   end
 
+  def update
+    type = params[:type]
+
+    if type.eql? 'drop'
+      @question.update(integration_params)
+    elsif type.eql? ''
+      @question.update(integration_params)
+    else
+      @question.update(integration_params)
+    end
+
+    render :index
+  end
+
   private
   def set_objects
-    @answers = Answer.where(subject_id: params[:subject_id])
-    @questions = Question.where(subject_id: params[:subject_id])
     @subjects = Subject.all
+    @questions = Question.where(subject_id: params[:subject_id])
+    @answers = if params.has_key?(:question_id) and params[:question_id] != ''
+                 Answer.where(subject_id: params[:subject_id])
+               else
+                 []
+               end
+  end
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def integration_params
+    params.require(:user).permit(:correct_answer_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,10 @@ Rails.application.routes.draw do
     resources :subjects, :path => 'assuntos'
     resources :questions, :path => 'questoes'
     resources :answers, :path => 'respostas'
-    post 'juntar', :controller => 'integrations', :action => 'index'
-    get 'juntar', :controller => 'integrations', :action => 'index'
+    post 'juntar' => 'integrations#index'
+    get 'juntar' => 'integrations#index'
 
+    post 'cms/juntar/:type/:correct_answer_id/:question_id' => 'integrations#update'
     get '/' => 'welcome#index'
   end
 
