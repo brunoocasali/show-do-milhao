6a27978cc70f75bb96fd94dd48ed0b614684cfb9
Bruno Casali <brunoocasali@gmail.com>
2014-11-07 10:16:04 -0200

    Rollbacking for the version that works!

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,12 +1,4 @@
 class Answer < ActiveRecord::Base
   belongs_to :subject
   has_and_belongs_to_many :questions
-
-  validate :must_have_3_answers_or_less
-
-  def must_have_3_answers_or_less
-    if self.questions_answers.size > 3
-      self.errors.add_to_base("Author is required to have 3 books or less")
-    end
-  end
 end
